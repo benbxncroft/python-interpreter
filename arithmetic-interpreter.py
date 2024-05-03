62f18fddde8c2c7db35d2d104d80638135e81242
Ben Bancroft <ben.bancroft@propelfinance.co.uk>
2024-05-03 16:02:13 +0100

    refactor and build code based on context-free grammars

--- arithmetic-interpreter.py
+++ arithmetic-interpreter.py
@@ -17,16 +17,11 @@ class Token:
     value: str | int
 
 
-class Interpreter:
+class Lexer:
     def __init__(self, user_input) -> None:
         self.user_input = user_input
         self.pos = 0
-        self.current_token = None
         self.current_char = self.user_input[self.pos]
-
-    def is_end_of_file(self) -> Token:
-        if self.pos >= len(self.user_input):
-            return Token(TokenTypes.EOF, None)
 
     def move_forward(self) -> None:
         self.pos += 1
@@ -46,74 +41,81 @@ class Interpreter:
             self.move_forward()
         return Token(TokenTypes.INTEGER, int(integer_value))
 
+    def is_end_of_file(self) -> Token:
+        if self.pos >= len(self.user_input):
+            return Token(TokenTypes.EOF, None)
+
     def get_token(self) -> Token:
         # lexical analyser
         # get token for each character
 
+        while self.current_char is not None:
+            self.skip_whitespace()
+
+            if self.current_char.isdigit():
+                return self.integer()
+
+            operations = {
+                "+": TokenTypes.PLUS,
+                "-": TokenTypes.MINUS,
+                "*": TokenTypes.MULTIPLY,
+                "/": TokenTypes.DIVIDE,
+            }
+
+            if self.current_char in operations:
+                token_value = self.current_char
+                self.move_forward()
+                return Token(operations[token_value], token_value)
+
         token = self.is_end_of_file()
-
-        self.current_char = self.user_input[self.pos]
-
-        self.skip_whitespace()
-
-        if self.current_char.isdigit():
-            return self.integer()
-
-        operations = {
-            "+": TokenTypes.PLUS,
-            "-": TokenTypes.MINUS,
-            "*": TokenTypes.MULTIPLY,
-            "/": TokenTypes.DIVIDE,
-        }
-
-        if self.current_char in operations:
-            self.pos += 1
-            return Token(operations[self.current_char], self.current_char)
 
         if token:
             return token
         else:
-            raise Exception("Error parsing input")
+            raise Exception("Error parsing input: get_token()")
 
-    def parse(self) -> None:
-        """
-        parses the tokenised string - the main entry point of this class
 
-        should recognise current token and build up a sequence
-        should then interpret what to do with that sequence
+class Interpreter:
+    def __init__(self, lexer) -> None:
+        self.lexer = lexer
+        self.current_token = self.lexer.get_token()
 
-        every time a token is read, call a method to get the next token
-        i.e. build the tokenised input up as it is interpreted
-        """
+    def eat(self, *args):
+        if self.current_token.token_type in args:
+            self.current_token = self.lexer.get_token()
+        else:
+            raise Exception("Error parsing input: eat()")
 
-        self.current_token = self.get_token()
+    def factor(self) -> str | int:
+        token = self.current_token
+        self.eat(TokenTypes.INTEGER)
+        return token.value
+
+    def expr(self) -> int:
+        result = self.factor()
 
         if self.current_token.token_type == TokenTypes.EOF:
             return
 
-        result = 0
-        if self.current_token.token_type == TokenTypes.INTEGER:
-            result = self.current_token.value
-            self.current_token = self.get_token()
-
-        def f(x, value):
-            return {
-                TokenTypes.PLUS: result + value,
-                TokenTypes.MINUS: result - value,
-                TokenTypes.MULTIPLY: result * value,
-                TokenTypes.DIVIDE: result / value,
-            }[x]
-
         while self.current_token.token_type in (
-            TokenTypes.PLUS,
-            TokenTypes.MINUS,
             TokenTypes.MULTIPLY,
             TokenTypes.DIVIDE,
+            TokenTypes.PLUS,
+            TokenTypes.MINUS,
         ):
-            operation = self.current_token
-            self.current_token = self.get_token()
-            value = self.current_token.value
-            result = f(operation.token_type, value)
+            token = self.current_token
+            if token.token_type == TokenTypes.MULTIPLY:
+                self.eat(TokenTypes.MULTIPLY)
+                result = result * self.factor()
+            elif token.token_type == TokenTypes.DIVIDE:
+                self.eat(TokenTypes.DIVIDE)
+                result = result / self.factor()
+            elif token.token_type == TokenTypes.PLUS:
+                self.eat(TokenTypes.PLUS)
+                result = result + self.factor()
+            elif token.token_type == TokenTypes.MINUS:
+                self.eat(TokenTypes.MINUS)
+                result = result - self.factor()
 
         return result
 
@@ -125,14 +127,20 @@ def main() -> None:
     * lexer that converts stream of input in to tokens
     * parser that parses those tokens in to a structure
     * interpreter that generates results from stream
+
+    based on the following grammar:
+
+    expr: factor((MULTIPLY|DIVIDE|PLUS|MINUS)factor)*
+    factor: INTEGER
     """
 
     while True:
         user_input = input("> ")
         if not user_input:
             continue
-        interpreter = Interpreter(user_input)
-        output = interpreter.parse()
+        lexer = Lexer(user_input)
+        interpreter = Interpreter(lexer)
+        output = interpreter.expr()
         print(output)
 
 
